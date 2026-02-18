f0113f6c97079ff2cc4bf35d6060ee420e37bc2a
Shubh25488 <shubh25488@iiitd.ac.in>
2026-02-18 16:27:52 +0530

    Update I_type.py

--- Simple_Assembler/I_type.py
+++ Simple_Assembler/I_type.py
@@ -9,12 +9,13 @@ def encode_i(mnemonic, rd_name, rs1_name,imm_inst):        #imm_inst=immediate i
         rs1=REGISTERS[rs1_name]
         val=int(imm_inst) 
         imm_bin=bin(val & 0xFFF)[2:].zfill(12)           #get 2's complement
-        binary_string=imm_bin+rs1+f3+rd+op               #total bit=12+5+3+5+7=32
+        binary_string=f"{imm_bin}{rs1}{f3}{rd}{op}               #total bit=12+5+3+5+7=32
         return binary_string
 
     except KeyError as e:
         return f"Error: Unknown mnemonic or register: {e}"
     
     
+
 
 #print(encode_r("addi","x1","x2","10"))
